d8ac207fa9750e6ba86b25b244c0e1c681ebcebb
Mr Chaotic <westberginc1@gmail.com>
2024-11-03 10:42:16 -0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
+function app(){
+  return `
+    <div class="header">
+      <h1>Deno Project</h1>
+    </div>
+  `;
+}
 
+Deno.serve({ port: 8000 }, async (_req) => {
+  return new Response(
+      [
+        
+      ].join(''),
+      {
+          status: 200,
+          headers: { "content-type": "text/html" },
+      }
+  );
+});
